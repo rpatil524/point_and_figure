206e2e767c4bbb016f57356be8b3ecff92e0b34c
Tsutomu Chikuba <chikuba.tsutomu@gmail.com>
2018-04-11 02:49:04 +0900

    temporary commit

--- lib/point_and_figure.rb
+++ lib/point_and_figure.rb
@@ -1,5 +1,77 @@
 require "point_and_figure/version"
 
 module PointAndFigure
-  # Your code goes here...
+  class << self
+    def generate(input_data)
+      base_point = input_data[:base_point]
+      base_turn = input_data[:base_turn]
+      data_set = input_data[:data_set]
+      output_data = {
+        base_point: base_point,
+        base_turn: base_turn,
+        data_set: []
+      }
+      prev_value = data_set[0]
+      current_trend = nil
+      current_line = nil
+      data_set.each.with_index(1) do |value, i|
+        next if i == 1
+        diff = (value - prev_value).round(1) # FIXME
+        if diff.abs < base_point
+          prev_value = value
+          next
+        end
+        if current_trend.nil?
+          current_trend = :up if diff >= base_point
+          current_trend = :down if diff.abs >= base_point && diff < 0
+        end
+        current_line ||= 1
+        if current_trend == :up
+          if diff.abs < base_point
+            prev_value = value
+            next
+          end
+          if diff >= base_point
+            output_data = up_data(base_point, value, prev_value, current_line, output_data)
+          elsif (diff.abs / base_point).round > base_turn
+            current_line += 1
+            output_data = down_data(base_point, value, prev_value, current_line, output_data, trend_changed: true)
+            current_trend = :down
+          end
+        elsif current_trend == :down
+          if diff.abs < base_point
+            prev_value = value
+            next
+          end
+          if diff.abs >= base_point && diff < 0
+            output_data = down_data(base_point, value, prev_value, current_line, output_data)
+          elsif (diff / base_point).round > base_turn
+            current_line += 1
+            output_data = up_data(base_point, value, prev_value, current_line, output_data, trend_changed: true)
+            current_trend = :up
+          end
+        end
+        prev_value = value
+      end
+      output_data
+    end
+
+    def up_data(base_point, value, prev_value, current_line, output_data, trend_changed: false)
+      flame = ((value - prev_value) / base_point).round(1) # FIXME
+      (0...flame).each do |i|
+        next if i == 0 && trend_changed
+        output_data[:data_set] << [current_line, (prev_value + base_point * i).round(1), (prev_value + base_point * (i + 1)).round(1)] # FIXME
+      end
+      output_data
+    end
+
+    def down_data(base_point, value, prev_value, current_line, output_data, trend_changed: false)
+      flame = ((value - prev_value).round(1) / base_point).abs # FIXME
+      (0...flame).each do |i|
+        next if i == 0 && trend_changed
+        output_data[:data_set] << [current_line, (prev_value - base_point * i).round(1), (prev_value - base_point * (i + 1)).round(1)] # FIXME
+      end
+      output_data
+    end
+  end
 end
--- test/point_and_figure_test.rb
+++ test/point_and_figure_test.rb
@@ -5,7 +5,75 @@ class PointAndFigureTest < Minitest::Test
     refute_nil ::PointAndFigure::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_it_returns_valid_json
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100, 100.1]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal input_data[:base_point], output_data[:base_point]
+    assert_equal input_data[:base_turn], output_data[:base_turn]
+    assert_equal [[1, 100, 100.1]], output_data[:data_set]
+  end
+
+  def test_it_returns_multi_flame_when_trend_is_up
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100, 100.2]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100, 100.1], [1, 100.1, 100.2]], output_data[:data_set]
+  end
+
+  def test_it_returns_multi_flame_when_trend_is_down
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100.2, 100]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100.2, 100.1], [1, 100.1, 100]], output_data[:data_set]
+  end
+
+  def test_it_turns_line_when_trend_changes_from_up_to_down
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100.3, 100.4, 100]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100.3, 100.4], [2, 100.3, 100.2], [2, 100.2, 100.1], [2, 100.1, 100]], output_data[:data_set]
+  end
+
+  def test_it_turns_line_when_trend_changes_from_down_to_up
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100.1, 100, 100.4]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100.1, 100], [2, 100.1, 100.2], [2, 100.2, 100.3], [2, 100.3, 100.4]], output_data[:data_set]
+  end
+
+  def test_it_doesnt_turn_line_when_trend_not_changed_from_up_to_down
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100.3, 100.4, 100.1]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100.3, 100.4]], output_data[:data_set]
+  end
+
+  def test_it_doesnt_turn_line_when_trend_not_changed_from_down_to_up
+    input_data = {
+      base_point: 0.1,
+      base_turn: 3,
+      data_set: [100.1, 100, 100.3]
+    }
+    output_data = PointAndFigure.generate input_data
+    assert_equal [[1, 100.1, 100]], output_data[:data_set]
   end
 end
